d0f8f19cde226e5c3ce6dfd36aded73b64d3ac40
Inworker <31877506+Inworker@users.noreply.github.com>
2024-07-08 00:40:13 +0300

    Поправил локаторы восстановления пароля

--- locators/forgot_page_locators.py
+++ locators/forgot_page_locators.py
@@ -2,5 +2,5 @@ from selenium.webdriver.common.by import By
 
 
 class ForgotPageLocators:
-    EMAIL_FIELD = (By.XPATH, "//input[@class='text input__textfield text_type_main-default']")  # Поле "Поле email"
+    EMAIL_FIELD = (By.XPATH, '*//input[@type = "text"]')  # Поле "Поле email"
     RESTORE_BUTTON = (By.XPATH, "//button[text()='Восстановить']")  # Кнопка "Восстановить"

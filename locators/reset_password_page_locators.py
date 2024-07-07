fee13eb48196f897978c2bc65735b45278f3a202
Inworker <31877506+Inworker@users.noreply.github.com>
2024-07-08 00:10:06 +0300

    Поправил локаторы сброса пароля

--- locators/reset_password_page_locators.py
+++ locators/reset_password_page_locators.py
@@ -3,4 +3,4 @@ from selenium.webdriver.common.by import By
 
 class ResetPasswordPageLocators:
     PASSWORD_FIELD = (By.XPATH, "//input[@name='Введите новый пароль']")  # Поле ввода "Введите новый пароль"
-    EYES_AREA = (By.XPATH, "//div[@class='input__icon input__icon-action']")  # Иконка скрыть/показать пароль
+    EYES_AREA = (By.XPATH, '//*[@class[contains(., "input__icon")]]')  # Иконка скрыть/показать пароль
